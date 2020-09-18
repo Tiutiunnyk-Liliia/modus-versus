e0a145501eb1d32c5381042669625692b4c52a29
Tiutiunnyk-Liliia <yalili4ka1210@gmail.com>
2020-09-18 12:53:07 +0300

    developing section "banner" is done

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,14 @@
 $(function () {
 
+    $('.banner-slider').slick({
+        prevArrow: '<button class="banner-slider__arrows banner-slider__arrow-prev">\n' +
+            '        <img src="img/arrow-left-slider-double.png" alt="">\n' +
+            '    </button>\n' ,
+        nextArrow: '<button class="banner-slider__arrows banner-slider__arrow-next">\n' +
+            '        <img src="img/arrow-right-slider-double.png" alt="">\n' +
+            '    </button>',
+        dots: true,
+    });
+
+
 });
